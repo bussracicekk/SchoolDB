acbeadfd549464e3f1d447c48126908dfb690048
Büşra ÇİÇEK <37375743+bussracicekk@users.noreply.github.com>
2020-09-20 01:32:15 +0300

    Add files via upload

---
+++ SchoolProject.Web/Controllers/OrganizationController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.DataAccess.Model;
+using SchoolProject.Facade.OrganizationFacade;
+
+
+namespace SchoolProject.Web.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        OrganizationFacade _organizationFacade;
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Organizations> organization = new List<Organizations>();
+            organization = _organizationFacade.GetAllOrganizations().ToList();
+            return View(organization);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(String Name)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Message"] = null;
+                TempData["Status"] = null;
+                var id = new int();
+                Organizations organization = new Organizations();
+                organization.Id = id;
+                organization.Name = Name;
+                _organizationFacade.AddNewOrganization(organization);
+                TempData["Message"] = "Add is not successfully";
+                TempData["Status"] = true;
+            }
+            else
+            {
+                TempData["Message"] = "Add is not successfully";
+                TempData["Status"] = false;
+            }
+            var result = _organizationFacade.GetAllOrganizations();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            IEnumerable <Organizations> organization = _organizationFacade.GetAllOrganizations(id);
+            if (organization == null)
+            {
+                return NotFound();
+            }
+            return View(organization);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind]Organizations organization)
+        {
+            if (id != organization.Id)
+            {
+                return null;
+            }
+            if (ModelState.IsValid)
+            {
+                _organizationFacade.EditOrganization(organization);
+                return RedirectToAction("Index");
+            }
+            return View(organization);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            IEnumerable <Organizations> organization = _organizationFacade.GetAllOrganizations(id);
+            if (organization == null)
+            {
+                return NotFound();
+            }
+            return View(organization);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _organizationFacade.DeleteOrganization(id);
+            return RedirectToAction("Index");
+        }
+    }
+    
+}
---
+++ SchoolProject.Web/Controllers/StudentController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.DataAccess.Model;
+using SchoolProject.Facade.OrganizationFacade;
+using SchoolProject.Facade.StudentFacade;
+
+namespace SchoolProject.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentFacade _studentFacade;
+        OrganizationFacade _organizationFacade;
+        // GET: /<controller>/
+        // string connectionString = 
+        public IActionResult Index()
+        {
+            List<Students> student = new List<Students>();
+            student = _studentFacade.GetAllStudents().ToList();
+            return View(student);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(string NameSurname,string orgName)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Message"] = null;
+                TempData["Status"] = null;
+                var id = new int();
+                Students student = new Students();
+                student.Id = id;
+                student.NameSurname = NameSurname;
+                student.OrganizationId =_organizationFacade.GetOrganizationIByName(orgName).Id;
+                _studentFacade.AddNewStudent(student);
+                TempData["Message"] = "Add is not successfully";
+                TempData["Status"] = true;
+            }
+            else
+            {
+                TempData["Message"] = "Add is not successfully";
+                TempData["Status"] = false;
+            }
+            var result = _studentFacade.GetAllStudents();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            IEnumerable<Students> student = _studentFacade.GetAllStudents(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind]Students student)
+        {
+            if (id != student.Id)
+            {
+                return null;
+            }
+            if (ModelState.IsValid)
+            {
+                _studentFacade.EditStudent(student);
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            IEnumerable<Students> student = _studentFacade.GetAllStudents(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _studentFacade.DeleteStudent(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
